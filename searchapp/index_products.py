b47008fb51ca34ffa192418a78560e44507d4c39
Aly Sivji <alysivji@gmail.com>
2018-08-06 14:14:11 -0500

    Finish lesson 1: index

--- searchapp/index_products.py
+++ searchapp/index_products.py
@@ -1,4 +1,5 @@
 from elasticsearch import Elasticsearch
+from elasticsearch.helpers import bulk
 
 from searchapp.constants import DOC_TYPE, INDEX_NAME
 from searchapp.data import all_products, ProductData
@@ -9,6 +10,7 @@ def main():
     es = Elasticsearch()
 
     es.indices.delete(index=INDEX_NAME, ignore=404)
+    es.indices.delete(index="mywords", ignore=404)
     es.indices.create(
         index=INDEX_NAME,
         body={
@@ -17,7 +19,19 @@ def main():
         },
     )
 
-    index_product(es, all_products()[0])
+    # index_product(es, all_products()[0])
+    bulk(es, products_to_index())
+
+    # def gendata():
+    #     mywords = ['foo', 'bar', 'baz']
+    #     for word in mywords:
+    #         yield {
+    #             "_index": "mywords",
+    #             "_type": "document",
+    #             "doc": {"word": word},
+    #         }
+
+    # bulk(es, gendata())
 
 
 def index_product(es, product: ProductData):
@@ -28,14 +42,27 @@ def index_product(es, product: ProductData):
         doc_type=DOC_TYPE,
         id=1,
         body={
-            "name": "A Great Product",
-            "image": "http://placekitten.com/200/200",
+            "name": product.name,
+            "image": product.image,
         }
     )
 
     # Don't delete this! You'll need it to see if your indexing job is working,
     # or if it has stalled.
     print("Indexed {}".format("A Great Product"))
+
+
+def products_to_index():
+    for product in all_products():
+        yield {
+            "_index": INDEX_NAME,
+            "_type": DOC_TYPE,
+            "_id": product.id,
+            "_source": {
+                "name": product.name,
+                "image": product.image,
+            }
+        }
 
 
 if __name__ == '__main__':
